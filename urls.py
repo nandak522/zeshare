ab82459105741dfbea16b9e2a1785d0d1d9055bf
Nanda Kishore <madhav.bnk@gmail.com>
2009-12-06 02:37:28 +0530

    My First Commit

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^zeshare/', include('zeshare.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    #(r'^admin/', include(admin.site.urls)),
+)
